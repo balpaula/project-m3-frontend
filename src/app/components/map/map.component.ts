7931f9a298b0f2a48ba1b9e7f2567a7bb33270e7
Marc Pósito <marcposito@gmail.com>
2018-08-14 17:05:04 +0200

    center map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-map',
@@ -7,11 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  isLoading = true;
+  map: any;
+  lat = 0;
+  lng = 0;
+
   constructor() { }
 
   ngOnInit() {
+      this.getPosition();
   }
 
+  getPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.isLoading = false;
+      this.lng = position.coords.longitude;
+      this.lat = position.coords.latitude;
+      // console.log([position.coords.longitude, position.coords.latitude]);
+      // return [position.coords.longitude, position.coords.latitude];
+      this.drawMap();
+    });
+  }
 
+  drawMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZXNib2FyZHMiLCJhIjoiY2prdHB4OTZuMDdtYjNrbGtvOGN1NGtqbyJ9.E8XQXS19fMbyyJY8PtiXaQ';
+    this.map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v10', // stylesheet location
+      center: [this.lng, this.lat] , // starting position [lng, lat]
+      zoom: 15 // starting zoom
+    });
+  }
 
 }
