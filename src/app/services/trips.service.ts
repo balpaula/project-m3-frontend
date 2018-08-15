d76aabe27d2b2c51600b13fd7d8b55fe35d50b91
Marc Pósito <marcposito@gmail.com>
2018-08-15 16:08:16 +0200

    before pull

--- src/app/services/trips.service.ts
+++ src/app/services/trips.service.ts
@@ -1,31 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '../../environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
 export class TripsService {
 
-  private API_URL = environment.API_URL;
-
   constructor( private httpClient: HttpClient ) { }
-
-  getTrips(): Promise<any> {
-    const options = {
-      withCredentials: true
-    };
-    return this.httpClient.get(`${this.API_URL}/trips/list`, options)
-      .toPromise()
-  }
-
-  createTrip(trip: any): Promise<any> {
-    const options = {
-      withCredentials: true
-    };
-    return this.httpClient.post(`${this.API_URL}/trips/new`, trip, options)
-      .toPromise()
-  }
-
 }
+
