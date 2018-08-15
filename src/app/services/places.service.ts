637f5aeb62bc279d6ce1ad6abb7efd463c852b91
Paula Balcells <paulabalcells@gmail.com>
2018-08-15 20:12:06 +0200

    trying addplace()

---
+++ src/app/services/places.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private API_URL = environment.API_URL;
+
+  constructor( private httpClient: HttpClient ) { }
+
+  addPlace(place: any, trip: any): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    console.log(place);
+    return this.httpClient.post(`${this.API_URL}/addplace`, place, options)
+      .toPromise()
+  }
+}
